1f5d78ce4990a673164b5a90cd56a7cfb28b3f79
Miguel Castañeda <miguelcataneda82@gmail.com>
2020-01-19 17:59:55 -0500

    Generator-MeanJS

---
+++ test/angular-service.test.js
@@ -0,0 +1,72 @@
+var path = require("path");
+var helpers = require("yeoman-generator").test;
+var assert = require("yeoman-generator").assert;
+var rimraf = require("rimraf");
+var temp = require("temp").track();
+
+describe("AngularJS Sub Generators Tests", function() {
+  this.timeout(0);
+
+  /**
+   * Setup the temp directory
+   */
+  before(function(done) {
+    helpers.testDirectory(path.join(__dirname, "temp"), done);
+  });
+
+  /**
+   * Clean up temp directory
+   */
+  after(function(done) {
+    temp.cleanup();
+
+    rimraf(path.join(__dirname, "temp"), done);
+  });
+
+  describe("Generate an AngularJS service file through the sub-generator", function() {
+    beforeEach(function(done) {
+      helpers
+        .run(path.join(__dirname, "../angular-service"))
+        .withOptions({
+          "skip-install": true
+        })
+        .withPrompts({
+          moduleName: "core",
+          name: "foo"
+        })
+        .on("ready", function(generator) {
+          // This is called right before `generator.run()` is called
+        })
+        .on("end", function() {
+          done();
+        });
+    });
+
+    it("should generate an angular service file", function() {
+      assert.file("modules/core/client/services/foo.client.service.js");
+    });
+  });
+
+  describe("Generate an AngularJS service file through the sub-generator (no name specified)", function() {
+    beforeEach(function(done) {
+      helpers
+        .run(path.join(__dirname, "../angular-service"))
+        .withOptions({
+          "skip-install": true
+        })
+        .withPrompts({
+          moduleName: "core"
+        })
+        .on("ready", function(generator) {
+          // This is called right before `generator.run()` is called
+        })
+        .on("end", function() {
+          done();
+        });
+    });
+
+    it("should generate an angular service file", function() {
+      assert.file("modules/core/client/services/core.client.service.js");
+    });
+  });
+});
